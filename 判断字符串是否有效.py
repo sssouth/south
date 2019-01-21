89a3284eee18720f6d1e9bc25a46fb2b4a1745d5
sssouth <1182394023@qq.com>
2019-01-21 13:09:55 +0800

    判断字符串是否有效

--- 判断字符串是否有效.py
+++ 判断字符串是否有效.py
@@ -7,8 +7,8 @@ else:
         print("%s是一个无效的字符串."%strin)
     else:
         for n in range(len(a_string)):
-            for i in range(len(a_string)-1):
-                if a_string[i]=='(' and a_string[i+2]==')' or a_string[i]=='[' and a_string[i+1]==']' or a_string[i]=='{' and a_string[i+1]=='}':
+            for i in range(len(a_string)-2):
+                if a_string[i]=='(' and a_string[i+1]==')' or a_string[i]=='[' and a_string[i+1]==']' or a_string[i]=='{' and a_string[i+1]=='}':
                     a_string = a_string[:i]+a_string[i+2:]              #字符串拼接
                     break
         if len(a_string)>2:
