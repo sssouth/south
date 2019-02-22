0fdaea832efe53e0ea435b00d5c824a1ea141824
sssouth <1182394023@qq.com>
2019-02-22 22:47:33 +0800

    画正方形

---
+++ 画正方形.py
@@ -0,0 +1,9 @@
+import turtle
+
+turtle.pencolor("black")    #画笔颜色
+turtle.pensize(2)   #画笔宽度
+for i in range(0,4):    #4次循环
+    turtle.forward(100) #当前方向移动100
+    turtle.right(90)    #画笔顺时针移动90
+turtle.done()
+    
